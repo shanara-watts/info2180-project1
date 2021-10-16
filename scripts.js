83a89b903c69868b52fbf4cc67646f4b748ce085
Shanara Watts <watts_shan2637@live.com>
2021-10-16 05:55:28 -0500

    Added new display files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+window.onload = function()
+{
+    btns = document.getElementsByClassName("btn")
+    var message = document.getElementsByClassName("message")
+    message = message[0]
+    btns[1].addEventListener("click", function(event){
+        var email = document.getElementsById("email").value
+        if(email == "")
+        {
+            message.innerHTML = "Please enter a valid email address"
+        }
+        else
+        {
+            message.innerHTML = "Thank you! Your email address"+ email +"has been added to our mailing list"
+        }
+        event.preventDefault()
+    })
+}
